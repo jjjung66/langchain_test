ed1c8c80a696c997566ad9d1b902b7f4e36fe543
Jongjin Jung <33506437+jjjung66@users.noreply.github.com>
2024-08-01 20:38:43 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -31,5 +31,5 @@ content = st.text_input('시의 주제를 제시해주세요.')
 
 if st.button('시 작성 요청하기'):
     with st.spinner('시 작성 중...'):
-        result = chatgpt.invoke(content + "에 대한 시를 써줘")
+        result = chatgpt.predict(content + "에 대한 시를 써줘")
         st.write(result)
