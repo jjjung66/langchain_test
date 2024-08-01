f24faaa0741b7c0b8629448c094aa87b9cbc3770
Jongjin Jung <33506437+jjjung66@users.noreply.github.com>
2024-08-01 19:46:22 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -5,6 +5,9 @@
 import os
 import streamlit as st
 
+#OPENAI API키 저장
+#os.environ["OPENAI_API_KEY"] = 'your key'
+
 #from langchain.llms import OpenAI
 #llm = OpenAI()
 
@@ -16,8 +19,8 @@ import streamlit as st
 from langchain_openai import ChatOpenAI
 
 chatgpt = ChatOpenAI(model_name="gpt-3.5-turbo", max_tokens = 512)
-answer = chatgpt.invoke("왜 파이썬이 가장 인기있는 프로그래밍 언어야?")
-print(answer.content)
+#answer = chatgpt.invoke("왜 파이썬이 가장 인기있는 프로그래밍 언어야?")
+#print(answer.content)
 
 import streamlit as st
 st.title('인공지능 시인')
@@ -27,5 +30,5 @@ content = st.text_input('시의 주제를 제시해주세요.')
 
 if st.button('시 작성 요청하기'):
     with st.spinner('시 작성 중...'):
-        result = chat_model.predict(content + "에 대한 시를 써줘")
+        result = chatgpt.invoke(content + "에 대한 시를 써줘")
         st.write(result)
